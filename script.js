95513b7f1e41dc63bfa3771fa940290c7b314389
fmenegolo <fmenegolo@hotmail.com>
2023-05-11 14:22:07 -0300

    novo

--- script.js
+++ script.js
@@ -1,58 +1,54 @@
-const tabelaHorarios = document.getElementById('tabela-horarios');
-const checkboxTurno = document.querySelectorAll('input[name="turno"]');
-
-// Função para preencher os horários de acordo com a escolha do turno
 function preencherHorarios(turno) {
-  // Limpa a tabela antes de preencher
-  tabelaHorarios.innerHTML = '';
-
-  const horarios = turno === 'manha-tarde' ?
-    ['07:00', '07:45', '08:30', '09:30', '10:15', '11:00', '12:30', '13:15', '14:00', '14:45', '15:30'] :
-    ['12:15', '13:00', '13:45', '14:15', '15:00', '15:45', '16:45', '17:30', '18:15', '19:45', '20:30'];
-
-  // Preenche a tabela com os horários
-  for (let i = 0; i < horarios.length; i++) {
-    const tr = document.createElement('tr');
-    const td1 = document.createElement('td');
-    const td2 = document.createElement('td');
-    const td3 = document.createElement('td');
-    const inputInicio = document.createElement('input');
-    const inputFim = document.createElement('input');
-    const inputObservacao = document.createElement('input');
-
-    inputInicio.type = 'time';
-    inputInicio.name = `horario-inicio-${i}`;
-    inputInicio.required = true;
-    inputFim.type = 'time';
-    inputFim.name = `horario-fim-${i}`;
-    inputFim.required = true;
-    inputObservacao.type = 'text';
-    inputObservacao.name = `observacao-${i}`;
-
-    td1.appendChild(inputInicio);
-    td2.appendChild(inputFim);
-    td3.appendChild(inputObservacao);
-
-    // Desabilita a edição dos horários de acordo com o turno escolhido
-    if (turno === 'manha-tarde' && i >= 9) {
-      inputInicio.disabled = true;
-      inputFim.disabled = true;
-    } else if (turno === 'tarde-noite' && i <= 5) {
-      inputInicio.disabled = true;
-      inputFim.disabled = true;
-    }
-
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    tabelaHorarios.appendChild(tr);
+  var horarios = document.getElementsByName("horario-inicio[]");
+  var fim = document.getElementsByName("horario-fim[]");
+  
+  if (turno === "manha-tarde") {
+    horarios[0].value = "07:00";
+    horarios[1].value = "07:45";
+    horarios[2].value = "08:30";
+    horarios[3].value = "09:30";
+    horarios[4].value = "10:15";
+    horarios[5].value = "11:00";
+    horarios[6].value = "12:30";
+    horarios[7].value = "13:15";
+    horarios[8].value = "14:00";
+    horarios[9].value = "14:45";
+    horarios[10].value = "15:30";
+    
+    fim[0].value = "07:45";
+    fim[1].value = "08:30";
+    fim[2].value = "09:15";
+    fim[3].value = "10:15";
+    fim[4].value = "11:00";
+    fim[5].value = "11:45";
+    fim[6].value = "13:15";
+    fim[7].value = "14:00";
+    fim[8].value = "14:45";
+    fim[9].value = "15:30";
+    fim[10].value = "16:00";
+  } else if (turno === "tarde-noite") {
+    horarios[0].value = "12:15";
+    horarios[1].value = "13:00";
+    horarios[2].value = "13:45";
+    horarios[3].value = "14:15";
+    horarios[4].value = "15:00";
+    horarios[5].value = "15:45";
+    horarios[6].value = "16:45";
+    horarios[7].value = "17:30";
+    horarios[8].value = "18:15";
+    horarios[9].value = "19:45";
+    horarios[10].value = "20:30";
+    
+    fim[0].value = "13:00";
+    fim[1].value = "13:45";
+    fim[2].value = "14:15";
+    fim[3].value = "15:00";
+    fim[4].value = "15:45";
+    fim[5].value = "16:30";
+    fim[6].value = "17:30";
+    fim[7].value = "18:15";
+    fim[8].value = "19:00";
+    fim[9].value = "20:30";
+    fim[10].value = "21:15";
   }
 }
-
-// Evento que chama a função para preencher os horários de acordo com o turno escolhido
-checkboxTurno.forEach((checkbox) => {
-  checkbox.addEventListener('change', () => {
-    const turno = document.querySelector('input[name="turno"]:checked').value;
-    preencherHorarios(turno);
-  });
-});
