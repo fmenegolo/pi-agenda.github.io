ce694def396472ae2cc7122f70755ad249eb500d
fmenegolo <fmenegolo@hotmail.com>
2023-05-31 23:26:19 -0300

    ddd

--- classes/Mysql.php
+++ classes/Mysql.php
@@ -9,7 +9,7 @@
                 try{
                     self::$pdo = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
                     self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                } catch(Exception $e){
+                    }catch(Exception $e){
                     echo '<h2 style="color:red;">Erro ao conectar com BD!</h2>';    
                 }
             }
